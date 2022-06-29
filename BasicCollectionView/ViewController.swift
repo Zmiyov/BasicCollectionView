5c5f732e619e930ad02c498d68e6eb2509fa6a6e
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-06-29 19:19:16 +0300

    Initial Commit

---
+++ BasicCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCollectionView
+//
+//  Created by Vladimir Pisarenko on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
