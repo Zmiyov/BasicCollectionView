3897f5a0401c9859d9cfbeddf76f3f75a9103eb6
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-06-29 19:48:29 +0300

    Simple compositional layout

---
+++ BasicCollectionView/BasicCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BasicCollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by Vladimir Pisarenko on 29.06.2022.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var textLabel: UILabel!
+    
+}
